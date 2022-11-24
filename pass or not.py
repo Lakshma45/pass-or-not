b02f4aedf6c9e5f6395929d3b2c4a1e429c81eee
Lakshma45 <111054201+Lakshma45@users.noreply.github.com>
2022-11-24 20:27:53 +0530

    Add files via upload

---
+++ pass or not.py
@@ -0,0 +1,11 @@
+sno=int(input('enter sno'))
+name=input('enter your name')
+group=input('enter your group name')
+s1=int(input('enter marks'))
+s2=int(input('enter marks'))
+s3=int(input('enter marks'))
+if s1<=30&s2<=30&s3<=30:
+ print('fail')
+else:
+ print('pass')
+ 
